73744556ebaaf34889e238d1610bee5c1312829e
kalebjorjino <20159238si@gmail.com>
2022-01-25 19:57:31 -0500

    responsibe mobilh11

--- src/components/Trabajo.vue
+++ src/components/Trabajo.vue
@@ -6,7 +6,7 @@
     
         <v-col lg="12" class="text-center">
            <v-subheader class="text-h4 justify-center">Trabajos Realizados</v-subheader>
-           <p>Son muchos los productos que podemos fabricar, casi de todo en madera y melamina.</p>
+           <p>Son muchos los productos que fabricamos, casi de todo en madera y melamina.</p>
         </v-col>
            <v-col cols="12">
                 <div class="text-center">
@@ -14,7 +14,7 @@
                     <v-chip @click="activeTrabajos = 'webapp'" :color="activeTrabajos === 'webapp'? 'success': ''" class="mx-5">Melamine</v-chip>
   
                     <v-chip @click="activeTrabajos = 'mobileapp'" :color="activeTrabajos === 'mobile'? 'success': ''" class="mx-5">Madera</v-chip>
-                    <v-chip @click="activeTrabajos = 'desktopapp'" :color="activeTrabajos === 'desktopapp'? 'success': ''" class="mx-5">Reparacion</v-chip>
+                    
      
                 </div>
   
@@ -62,27 +62,7 @@
            </v-card>       
         </v-col>
         </slot>
-         <slot v-if="activeTrabajos === 'desktopapp'">
-        <v-col lg="4" v-for="(desktop,desktopindex) in desktopApps" :key="desktopindex">
-           <v-card>
-             <v-img 
-                  
-                       :src="desktop.src"
-                       class="white--text align-end"
-                       height="200px"
-            >
-                <v-card-title>{{desktop.title}}</v-card-title>
-            </v-img> 
-            <v-card-subtitle class="pb-0">{{ desktop.subtitle }}</v-card-subtitle>
-            <v-card-text class="text--primary">
-              <div>{{ desktop.description }}</div>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn color="primary" text>Ver</v-btn>
-            </v-card-actions>    
-           </v-card>       
-        </v-col>
-        </slot>
+      
     
         </v-row>
     
@@ -137,28 +117,8 @@ export default {
                 }
     
                
-            ],
-             desktopApps: [
-                { title: 'Mueble 4', 
-                  subtitle: "Mueble en melamine",
-                  description: "Realizamos diseños y fabricaciones de reposteros de melamina o madera, en colores solicitados por nuestros clientes.",
-                  src: require("../assets/images/photo01.jpeg")                               
-                },
-
-                { title: 'Mueble 5', 
-                  subtitle: "Mueble en melamine",
-                  description: "Realizamos diseños y fabricaciones de reposteros de melamina o madera, en colores solicitados por nuestros clientes.",
-                  src: require("../assets/images/photo02.jpeg") 
-                },
-
-                { title: 'Mueble 6', 
-                  subtitle: "Mueble en melamine",
-                  description: "Realizamos diseños y fabricaciones de reposteros de melamina o madera, en colores solicitados por nuestros clientes.",
-                  src: require("../assets/images/photo03.jpeg") 
-                }
-    
-               
             ]
+           
 
         }
     }
